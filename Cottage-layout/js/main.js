f72064381f81ef7bdd2dea855e71e735456cf22b
Arseniy Suschevskiy <58954499+ArseniySushCorp@users.noreply.github.com>
2020-05-04 23:33:24 +0300

    Add new Cottage project

---
+++ Cottage-layout/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt="prev arrow"></button>',
+        responsive: [
+            {
+              breakpoint: 500,
+              settings: {
+                arrows:false
+              }
+            }]
+    });
+});
