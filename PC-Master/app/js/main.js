ce76b44ba89e9959cd7ec89760d5b6cbcef1a2ea
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2020-11-20 14:40:35 +0300

    Add animation to benefits section

--- PC-Master/app/js/main.js
+++ PC-Master/app/js/main.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('.header')
 const aboutSection = document.querySelector('.about')
 const headerNav = document.querySelector('.header__nav')
+const benefitsSection = document.querySelector('.benefits')
+const benefitsItem = document.querySelectorAll('.benefits__item')
 
 document.addEventListener('scroll', () => {
 	if (window.scrollY >= aboutSection.clientHeight/2) {
@@ -10,5 +12,15 @@ document.addEventListener('scroll', () => {
 	else {
 		header.style.backgroundColor = 'transparent'
 		headerNav.style.paddingTop = '75px'
+	}
+
+	if (window.scrollY >= benefitsSection.clientHeight ){
+		benefitsItem.forEach((item, i) => {
+			if (i % 2 === 0) {
+				item.classList.add('benefits-animation')
+			} else {
+				item.classList.add('benefits-animation-right')
+			}
+		})
 	}
 })
