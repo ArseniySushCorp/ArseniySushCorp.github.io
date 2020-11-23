83d303fed15aea532229197882492bc7035855b7
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2020-11-23 23:31:28 +0300

    delete send.php

--- PC-Master/app/js/main.js
+++ PC-Master/app/js/main.js
@@ -80,5 +80,3 @@ let selector = document.querySelectorAll('input[type="tel"]')
 let im = new Inputmask('+7(999)999-99-99')
 im.mask(selector)
 
-const TGtoken = '1411277987:AAFXqqy-h99u6OTjdhw_uW0DRgg8AEoCDds'
-const chatId = '-363813984'
